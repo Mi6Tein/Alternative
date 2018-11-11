6de6c29d471d8b4ee727312b2d99ab24efb2dc48
Mi6Tein <43059812+Mi6Tein@users.noreply.github.com>
2018-11-11 15:24:43 +0300

    Rename h2.cpp to hw4.cpp

---
+++ hw4.cpp
@@ -0,0 +1,21 @@
+﻿//4) Напишите программу, определяющую количество нулей в числе N! Известно, что N помещается в тип unsigned int. 
+#include "pch.h"
+#include <iostream>
+
+int f(int n) {
+	int k = 0;
+	for (int i = 1; i <= n; i++) {
+		if (i % 5 == 0) k++;
+	}
+	return k;
+}
+
+
+int main()
+{
+	unsigned N;
+	std::cin >> N;
+	std::cout << f(N) << std::endl;
+		system("pause");
+	return 0;
+}
