adfc2ac6fe9fde1d7efa207999611c1aa0ee1eb7
Mi6Tein <43059812+Mi6Tein@users.noreply.github.com>
2018-11-11 15:51:39 +0300

    Update and rename hw4.cpp to h2.cpp

---
+++ h2.cpp
@@ -0,0 +1,19 @@
+﻿//2) Напишите программу, вычисляющую сумму нечетных цифр целого числа
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	unsigned N;
+	std::cin >> N;
+	int sum = 0;
+	while (N / 10 != 0 || N % 10 !=0) {
+		if (N % 10 % 2 == 1) {
+			sum += N % 10;
+		}
+		N /= 10;
+	}
+	std::cout << sum << " " << std::endl;
+	system("pause");
+	return 0;
+}
